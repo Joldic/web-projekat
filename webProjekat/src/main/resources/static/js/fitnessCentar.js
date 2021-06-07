166d524de9396dc561b37151d3b02c5712823645
Joldic <wotnenad@gmail.com>
2021-06-07 19:53:00 +0200

    dodavanje fitness centra

--- webProjekat/src/main/resources/static/js/fitnessCentar.js
+++ webProjekat/src/main/resources/static/js/fitnessCentar.js
@@ -8,13 +8,13 @@ $(document).ready(function(){
             console.log("SUCCESS:\n", res);
             for(i=0; i < res.length; i++){
                 let row = "<tr>";
-                row += "<td>" + res[i].naziv + "<td>";
-                row += "<td>" + res[i].adresa + "<td>";
-                row += "<td>" + res[i].brojTelefonaCentrale + "<td>";
-                row += "<td>" + res[i].eMail + "<td>";
+                row += "<td>" + res[i].naziv + "</td>";
+                row += "<td>" + res[i].adresa + "</td>";
+                row += "<td>" + res[i].brojTelefonaCentrale + "</td>";
+                row += "<td>" + res[i].eMail + "</td>";
 
-                let btn = "<button class=btnSeeMore data-id=" + res[i].id + ">SeeMore</button>";
-                row += "<td>" + btn + "<td>";
+               // let btn = "<button class=btnSeeMore data-id=" + res[i].id + ">SeeMore</button>";
+                //row += "<td>" + btn + "<td>";
                 row += "</tr>";
                 $('#fitnesscentar').append(row);
             }
@@ -23,4 +23,39 @@ $(document).ready(function(){
             console.log("ERROR:\n", res);
         }
     });
+});
+
+//Kreiranje novog zaposlenog
+$(document).on("submit", "form", function(event){
+    event.preventDefault();
+
+    var naziv = $("#naziv").val();
+    var adresa = $("#adresa").val();
+    var brojTelefonaCentrale = $("#brojTelefonaCentrale").val();
+    var eMail = $("#eMail").val();
+
+    var newFitnessCentar = {
+        naziv,
+        adresa,
+        brojTelefonaCentrale,
+        eMail
+    }
+    console.log(newFitnessCentar);
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8090/api/fitnesscentar",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(newFitnessCentar),
+        success: function (res){
+            console.log(res);
+            alert("Fitnes centar je uspesno kreairan");
+            window.location.href = "fitnessCentar.html";
+        },
+        error: function(){
+            alert("Greska!");
+        }
+    });
+
 });
