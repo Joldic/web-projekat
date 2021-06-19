47ddbfa761042af727b554166763a249c0e9b1ce
Joldic <wotnenad@gmail.com>
2021-06-19 02:55:59 +0200

    add fitness popravljen

---
+++ webProjekat/src/main/resources/static/js/addFitness.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    var korId = localStorage.getItem("id");
+
+    $.ajax({
+        type: "GET",
+        url:"http://localhost:8090/api/korisnik/Uloga/" + korId,
+        dataType: "json",
+        contentType: "application/json",
+        success: function (res){
+            console.log(res);
+            alert("admin je na stranici");
+        },
+        error: function (){
+            alert("Greska niste ADMIN");
+            window.location.href = "index.html";
+        }
+    });
+});
+
+
+
+
+
+
+//kreiranje novog fitness centra
+$(document).on("submit", "form", function(event){
+    event.preventDefault();
+
+    var naziv = $("#naziv").val();
+    var adresa = $("#adresa").val();
+    var brojTelefonaCentrale = $("#brojTelefonaCentrale").val();
+    var eMail = $("#eMail").val();
+
+    var newFitnessCentar = {
+        naziv,
+        adresa,
+        brojTelefonaCentrale,
+        eMail
+    }
+    console.log(newFitnessCentar);
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8090/api/fitnesscentar",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(newFitnessCentar),
+        success: function (res){
+            console.log(res);
+            alert("Fitnes centar je uspesno kreairan");
+            window.location.href = "fitnessCentar.html";
+        },
+        error: function(){
+            alert("Greska!");
+        }
+    });
+
+});
--- webProjekat/src/main/resources/static/js/fitnessCentar.js
+++ webProjekat/src/main/resources/static/js/fitnessCentar.js
@@ -26,58 +26,61 @@ $(document).ready(function(){
 });
 
 
+// $(document).on('click', '.addId', function(){
+//     var korId = localStorage.getItem(("id"));
+//     console.log(localStorage.getItem("id"));
+//
+//     $.ajax({
+//         type: "GET",
+//         url:"http://localhost:8090/api/korisnik/Uloga/" + korId,
+//         dataType: "json",
+//         contentType: "application/json",
+//         success: function (res){
+//             console.log(res);
+//             alert("admin je na stranici");
+//             window.location.href = "add-fintessCentar.html";
+//         },
+//         error: function (){
+//             alert("Greska niste ADMIN");
+//         }
+//     });
+//
+// });
 $(document).on('click', '.addId', function(){
-    var korId = localStorage.getItem(("id"));
-    console.log(localStorage.getItem("id"));
-
-    $.ajax({
-        type: "GET",
-        url:"http://localhost:8090/api/korisnik/Uloga/" + korId,
-        dataType: "json",
-        contentType: "application/json",
-        success: function (res){
-            console.log(res);
-            alert("admin je na stranici");
-            window.location.href = "add-fintessCentar.html";
-        },
-        error: function (){
-            alert("Greska niste ADMIN");
-        }
-    });
-
+   window.location.href = "add-fintessCentar.html";
 });
 
 //Kreiranje novog fitness centra
-$(document).on("submit", "form", function(event){
-    event.preventDefault();
-
-    var naziv = $("#naziv").val();
-    var adresa = $("#adresa").val();
-    var brojTelefonaCentrale = $("#brojTelefonaCentrale").val();
-    var eMail = $("#eMail").val();
-
-    var newFitnessCentar = {
-        naziv,
-        adresa,
-        brojTelefonaCentrale,
-        eMail
-    }
-    console.log(newFitnessCentar);
-
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:8090/api/fitnesscentar",
-        dataType: "json",
-        contentType: "application/json",
-        data: JSON.stringify(newFitnessCentar),
-        success: function (res){
-            console.log(res);
-            alert("Fitnes centar je uspesno kreairan");
-            window.location.href = "fitnessCentar.html";
-        },
-        error: function(){
-            alert("Greska!");
-        }
-    });
-
-});
+// $(document).on("submit", "form", function(event){
+//     event.preventDefault();
+//
+//     var naziv = $("#naziv").val();
+//     var adresa = $("#adresa").val();
+//     var brojTelefonaCentrale = $("#brojTelefonaCentrale").val();
+//     var eMail = $("#eMail").val();
+//
+//     var newFitnessCentar = {
+//         naziv,
+//         adresa,
+//         brojTelefonaCentrale,
+//         eMail
+//     }
+//     console.log(newFitnessCentar);
+//
+//     $.ajax({
+//         type: "POST",
+//         url: "http://localhost:8090/api/fitnesscentar",
+//         dataType: "json",
+//         contentType: "application/json",
+//         data: JSON.stringify(newFitnessCentar),
+//         success: function (res){
+//             console.log(res);
+//             alert("Fitnes centar je uspesno kreairan");
+//             window.location.href = "fitnessCentar.html";
+//         },
+//         error: function(){
+//             alert("Greska!");
+//         }
+//     });
+//
+// });
